174628fe526edb5b83063044cf2d42a551a17914
Rita Pasztor <pasztorrita@gmail.com>
2016-11-14 14:09:51 +0100

    Workshop03 - getters and setter added

---
+++ week-05/Day-01/src/Workshop03.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Rita on 2016.11.14..
+ */
+public class Workshop03 {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.setColor("red");
+        myCar.setEngineSize(1600);
+        myCar.setTypeCar("Mazda");
+        myCar.setKmOdometer(12312);
+
+        Car myCar2 = new Car();
+        myCar2.setColor("silver");
+        myCar2.setEngineSize(2500);
+        myCar2.setTypeCar("Ford");
+        myCar2.setKmOdometer(152312);
+
+        Car myCar3 = new Car();
+        myCar3.setColor("green");
+        myCar3.setEngineSize(2800);
+        myCar3.setTypeCar("Beamer");
+        myCar3.setKmOdometer(42521);
+
+
+        System.out.printf("This %s is %s, has %s cc engine and clocked %s km's.\n", myCar.getTypeCar(), myCar.getColor(), myCar.getEngineSize(), myCar.getKmOdometer());
+        System.out.printf("This %s is %s, has %s cc engine and clocked %s km's.\n", myCar2.getTypeCar(), myCar2.getColor(), myCar2.getEngineSize(), myCar2.getKmOdometer());
+        System.out.printf("This %s is %s, has %s cc engine and clocked %s km's.\n", myCar3.getTypeCar(), myCar3.getColor(), myCar3.getEngineSize(), myCar3.getKmOdometer());
+
+    }
+}
