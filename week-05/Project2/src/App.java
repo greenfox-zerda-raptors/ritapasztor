ae511a822cdb91b6fbc17d503182a55caadc36df
Rita Pasztor <pasztorrita@gmail.com>
2016-11-18 10:31:40 +0100

    TodoListProject Friday morning - working version

--- week-05/Project2/src/App.java
+++ week-05/Project2/src/App.java
@@ -1,3 +1,5 @@
+package TodoListProject;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,14 +11,14 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
-//       try( BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Rita\\Documents\\greenfox\\ritapasztor\\week-05\\Project\\src\\com\\greenfox\\RitaTodoList\\command.txt"))) {
-//           String Currentline;
-//           while ((Currentline = br.readLine()) !=null) {
-//               System.out.println(Currentline);
-//           }
-//       } catch (IOException e) {
-//           e.printStackTrace();
-//       }
+       try( BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Rita\\Documents\\greenfox\\ritapasztor\\week-05\\Project\\src\\com\\greenfox\\RitaTodoList\\command.txt"))) {
+           String Currentline;
+           while ((Currentline = br.readLine()) !=null) {
+               System.out.println(Currentline);
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
 
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
