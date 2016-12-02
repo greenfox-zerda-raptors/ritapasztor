8f5ac0841fa83674ab500d5109cbaaca12d0d7c9
Rita Pasztor <pasztorrita@gmail.com>
2016-12-02 17:31:06 +0100

    add array and arrayList

---
+++ Week-06/ObjectOrientation/src/Workshop01Array.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Rita on 2016.12.02..
+ */
+public class Workshop01Array {
+    public static void main(String[] args) {
+        String[] colors = new String[]{"white", "blue", "red", "silver", "black", "green", "banana"};
+        String[] makes = new String[]{"toyota", "mazda", "bmw", "vw", "opel", "suzuki", "ford", "mercedes"};
+        Integer[] engineSizes = new Integer[]{1100, 1400, 1600, 1800, 2500};
+
+        ArrayList<Car> lsCars = new ArrayList<>(10);
+
+
+    for (int i = 0; i < 10; i++) {
+
+        Random randomColor = new Random();
+
+
+
+    }
+    }
+}
