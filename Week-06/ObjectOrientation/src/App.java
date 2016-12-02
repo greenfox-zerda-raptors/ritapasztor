94186f58cd1fd5e704447122ec56bcce8cf84836
Rita Pasztor <pasztorrita@gmail.com>
2016-12-02 15:46:19 +0100

    add constructor, create toString method, print cars

--- Week-06/ObjectOrientation/src/App.java
+++ Week-06/ObjectOrientation/src/App.java
@@ -3,8 +3,12 @@
  */
 public class App {
     public static void main(String[] args) {
-        Car myCar = new Car();
-        System.out.printf("Just got a new Car, it's %s and has a %d cc Engine."
-                ,myCar.color,myCar.engineSize);
+        Car myCar1 = new Car(1600, "red", "Mazda", 12312);
+        Car myCar2 = new Car(2500, "silver", "Ford", 152312);
+        Car myCar3 = new Car(2800, "green", "Beamer", 42521);
+
+        System.out.println(myCar1.toString());
+        System.out.println(myCar2.toString());
+        System.out.println(myCar3.toString());
     }
 }
--- Week-06/ObjectOrientation/src/Car.java
+++ Week-06/ObjectOrientation/src/Car.java
@@ -6,4 +6,19 @@ public class Car {
     String color = "red";
     String typeCar = "Mazda";
     int kmOdometer;
+
+    public Car(int engineSize, String color, String typeCar, int kmOdometer) {
+        this.engineSize = engineSize;
+        this.color = color;
+        this.typeCar = typeCar;
+        this.kmOdometer = kmOdometer;
+    }
+
+    public String toString() {
+        return String.format("This %s is %s, has %d cc engine and clocked %d km's.",
+                this.typeCar,
+                this.color,
+                this.engineSize,
+                this.kmOdometer);
+    }
 }
