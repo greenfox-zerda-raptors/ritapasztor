1902eeac9766e9f66a95218b0ee3b279959f5de7
Rita Pasztor <pasztorrita@gmail.com>
2016-12-11 19:24:11 +0100

    add deposit, transfer methods

--- week-07/BankingApplication/src/Account.java
+++ week-07/BankingApplication/src/Account.java
@@ -2,11 +2,11 @@
  * Created by Rita on 2016.12.11..
  */
 public abstract class Account {
-    int accountID;
-    String firstName;
-    String lastName;
-    String address;
-    double balance;
+    private int accountID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private double balance;
 
     public Account(int accountID, String firstName, String lastName, String address) {
         balance = 0;
@@ -19,9 +19,32 @@ public abstract class Account {
 
     public abstract void audit();
 
-    public double deposit(double depositAmount) {
+    public void deposit(int depositAmount) {
+        balance += depositAmount;
+        System.out.println("On " + firstName + "'s account, new balance is " + balance + " EUR");
+    }
+
+    public double getBalance() {
         return balance;
     }
 
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " type " +
+                "account no. " + accountID +
+                ", " + firstName + " " + lastName + " " +
+                ", account balance = " + balance + " EUR.";
+    }
+
+
+    public void transfer(int transferredAmount, Account accountTo) {
+        this.balance -= transferredAmount;
+        accountTo.deposit(transferredAmount);
+    }
 }
 
--- week-07/BankingApplication/src/Bank.java
+++ week-07/BankingApplication/src/Bank.java
@@ -1,15 +1,50 @@
+import java.util.ArrayList;
+
 /**
  * Created by Rita on 2016.12.11..
  */
 public class Bank {
     public static void main(String[] args) {
-        CheckingAccount checkingAccount1;
-        CheckingAccount checkingAccount2;
-        SavingsAccount savingsAccount1;
-        SavingsAccount savingsAccount2;
-        MoneyMarket moneyMarket1;
-        MoneyMarket moneyMarket2;
+        ArrayList<Account> accounts = new ArrayList<>();
 
-        
+        CheckingAccount checkingAccount1 = new CheckingAccount(01, "Szende", "Torpe", "Erdo utca");
+        accounts.add(checkingAccount1);
+        CheckingAccount checkingAccount2 = new CheckingAccount(02, "Szundi", "Torpe", "Mezo utca");
+        accounts.add(checkingAccount2);
+        SavingsAccount savingsAccount1 = new SavingsAccount(03, "Hapci", "Torpe", "Bokor utca");
+        accounts.add(savingsAccount1);
+        SavingsAccount savingsAccount2 = new SavingsAccount(04, "Morgo", "Torpe", "Fa utca");
+        accounts.add(savingsAccount2);
+        MoneyMarket moneyMarket1 = new MoneyMarket(05, "Kuka", "Torpe", "Virag ter");
+        accounts.add(moneyMarket1);
+        MoneyMarket moneyMarket2 = new MoneyMarket(06, "Tudor", "Vidor", "Banya koz");
+        accounts.add(moneyMarket2);
+
+        for (Account account : accounts) {
+            System.out.println(account.toString());
+        }
+
+        checkingAccount1.deposit(1000);
+        checkingAccount2.deposit(2000);
+        savingsAccount1.deposit(500);
+        System.out.println(savingsAccount2.getBalance());
+        moneyMarket1.deposit(5000);
+        moneyMarket2.deposit(4500);
+
+        moneyMarket1.transfer(2000, savingsAccount2);
+        System.out.println(moneyMarket1.getBalance());
+        System.out.println(savingsAccount2.getBalance());
+
+        for (Account account : accounts) {
+            System.out.println(account.toString());
+        }
+
+        System.out.println();
+
+        checkingAccount2.transfer(1200, savingsAccount1);
+        System.out.println(checkingAccount2.getBalance());
+        System.out.println(savingsAccount1.getBalance());
+
+
     }
 }
