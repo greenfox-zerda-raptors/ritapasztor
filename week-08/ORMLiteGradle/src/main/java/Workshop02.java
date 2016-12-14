485739999a3962dc5f1d17a1d2f4bcac72e2499c
Rita Pasztor <pasztorrita@gmail.com>
2016-12-14 16:58:20 +0100

    workshop02 done, nicely printed

--- week-08/ORMLiteGradle/src/main/java/Workshop02.java
+++ week-08/ORMLiteGradle/src/main/java/Workshop02.java
@@ -1,17 +1,20 @@
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Rita on 2016.12.14..
  */
 public class Workshop02 {
     public static void main(String[] args) throws SQLException {
-        String databaseUrl = "jdbc:mysql:localhost:3306/workshop02?user=root&password=R1tcsi";
+        String databaseUrl = "jdbc:mysql://localhost:3306/workshop02?user=root&password=R1tcsi&serverTimezone=UTC";
 
         ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
 
@@ -24,6 +27,27 @@ public class Workshop02 {
 
         account = accountDao.queryForId("Captain America");
         System.out.println("Account: " + account.toString());
+
+        ArrayList<Account> accountNames = new ArrayList<>();
+        accountNames.add(new Account("Iron Man", "1231"));
+        accountNames.add(new Account("Wolverine", "1232"));
+        accountNames.add(new Account("Hulk", "1233"));
+        accountNames.add(new Account("Loki", "1234"));
+
+        for (Account hero : accountNames) {
+            createAccountIfNotExists(accountDao, hero);
+        }
+
+        try {
+            QueryBuilder<Account, String> queryBuilder = accountDao.queryBuilder();
+            System.out.println(accountDao.queryForAll());
+            List<Account> accountList = accountDao.queryBuilder().orderBy("name", true).query();
+            for (Account acc : accountList) {
+                System.out.println(acc);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void createAccountIfNotExists(Dao<Account, String> accountDao, Account acc) throws SQLException {
