29a499538afd470a79b3c9b087341627f8bd396d
Rita Pasztor <pasztorrita@gmail.com>
2016-12-14 14:02:17 +0100

    setup gradle project, add gitignore, copy workshops

---
+++ week-08/ORMLiteGradle/src/main/java/Account.java
@@ -0,0 +1,37 @@
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Created by Rita on 2016.12.14..
+ */
+@DatabaseTable(tableName = "Account")
+public class Account {
+    @DatabaseField(id = true)
+    String name;
+    @DatabaseField
+    String password;
+
+    public Account() {
+    }
+
+    public Account(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
