ba9394c6a0c7b0577c879312a0b41d80eb462ec0
Rita Pasztor <pasztorrita@gmail.com>
2016-12-15 08:50:01 +0100

    add aircraft class and methods

---
+++ week-08/RetakePracticeGradle/src/main/java/com/greenfox/exams/java/retake/Aircraft.java
@@ -0,0 +1,33 @@
+package com.greenfox.exams.java.retake;
+
+/**
+ * Created by Rita on 2016.12.15..
+ */
+public abstract class Aircraft {
+    private int currentAmmo;
+
+    public Aircraft() {
+        currentAmmo = 0;
+    }
+
+    public abstract int getbaseDamage();
+
+    public abstract int getmaxAmmo();
+
+    public int fight() {
+        int damage = currentAmmo * getbaseDamage();
+        currentAmmo = 0;
+        return damage;
+    }
+
+    public int refill(int fillingAmmo) {
+        int ammoNeeded = getmaxAmmo() - currentAmmo;
+        if (ammoNeeded >= fillingAmmo) {
+            currentAmmo += fillingAmmo;
+            return 0;
+        } else {
+            currentAmmo = getmaxAmmo();
+            return fillingAmmo - ammoNeeded;
+        }
+    }
+}
---
+++ week-08/RetakePracticeGradle/src/main/java/com/greenfox/exams/java/retake/F16.java
@@ -0,0 +1,14 @@
+package com.greenfox.exams.java.retake;
+
+/**
+ * Created by Rita on 2016.12.15..
+ */
+public class F16 extends Aircraft {
+    public int getbaseDamage() {
+        return 30;
+    }
+
+    public int getmaxAmmo() {
+        return 8;
+    }
+}
