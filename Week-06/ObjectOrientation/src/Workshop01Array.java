323bc2914eaa955580e7e7e34825cbab4278c02c
Rita Pasztor <pasztorrita@gmail.com>
2016-12-15 10:01:21 +0100

    add Worhshop01

--- Week-06/ObjectOrientation/src/Workshop01Array.java
+++ Week-06/ObjectOrientation/src/Workshop01Array.java
@@ -5,17 +5,19 @@ import java.util.Random;
  * Created by Rita on 2016.12.02..
  */
 public class Workshop01Array {
-    public static void main(String[] args) {
-        String[] colors = new String[]{"white", "blue", "red", "silver", "black", "green", "banana"};
-        String[] makes = new String[]{"toyota", "mazda", "bmw", "vw", "opel", "suzuki", "ford", "mercedes"};
-        Integer[] engineSizes = new Integer[]{1100, 1400, 1600, 1800, 2500};
+    String[] colors = new String[]{"white", "blue", "red", "silver", "black", "green", "banana"};
+    String[] makes = new String[]{"toyota", "mazda", "bmw", "vw", "opel", "suzuki", "ford", "mercedes"};
+    Integer[] engineSizes = new Integer[]{1100, 1400, 1600, 1800, 2500};
 
-        ArrayList<Car> lsCars = new ArrayList<>(10);
+    ArrayList<Car> lsCars = new ArrayList<>(10);
+
+    Random randomCar = new Random();
+    public static void main(String[] args) {
+
 
 
     for (int i = 0; i < 10; i++) {
 
-        Random randomColor = new Random();
 
 
 
