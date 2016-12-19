b594d25f168f173c76b3532b9abccc801fb63175
Rita Pasztor <pasztorrita@gmail.com>
2016-12-19 14:11:55 +0100

    counter added

--- week-09/Springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-09/Springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,15 +4,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by Rita on 2016.12.19..
  */
 @RestController
 public class HelloRESTController {
     Greeting greeting;
+    AtomicLong atomicLong = new AtomicLong();
 
     @RequestMapping
     public Greeting greeting(@RequestParam("name") String name) {
-        return new Greeting(19, "Hello, " + name);
+        long val = atomicLong.incrementAndGet();
+        return new Greeting(val, " , Hello, " + name);
     }
+
+
 }
