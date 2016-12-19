2d0c5daeeb3bf620df26101e7db39e225cff3d00
Rita Pasztor <pasztorrita@gmail.com>
2016-12-19 14:49:15 +0100

    web app greeting me and counting

--- week-09/Springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-09/Springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,6 +3,9 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 
 /**
@@ -10,10 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HelloWebController {
+    AtomicLong atomicLong = new AtomicLong();
 
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
+    public String greeting(Model model, @RequestParam("name") String name) {
+
+        long val = atomicLong.incrementAndGet();
+        model.addAttribute("name", name);
+        model.addAttribute("val", val);
         return "greeting";
     }
 }
