a098ad4ff421202c668524ee517718ec7aaec4c7
Rita Pasztor <pasztorrita@gmail.com>
2016-12-22 13:46:08 +0100

    Database practice Workshop01

---
+++ week-09/Databases/src/main/java/com/greenfox/Workshop02Application.java
@@ -0,0 +1,12 @@
+package com.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop02Application.class, args);
+	}
+}
