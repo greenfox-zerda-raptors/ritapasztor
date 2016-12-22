14459d442cf5e20b96e87f01bb1eeb6001c0fe2a
Rita Pasztor <pasztorrita@gmail.com>
2016-12-22 14:14:32 +0100

    workshop02

---
+++ week-09/WorkShopNExt/src/main/java/com/example/Customer.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * Created by Rita on 2016.12.22..
+ */
+
+@Entity
+
+@Table(name = "customers")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+}
---
+++ week-09/WorkShopNExt/src/main/java/com/example/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by Rita on 2016.12.22..
+ */
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+}
