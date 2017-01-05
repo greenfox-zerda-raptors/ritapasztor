0bdd54c011a0cd852840e4b3df5d2c72df10b757
Rita Pasztor <pasztorrita@gmail.com>
2017-01-05 17:29:52 +0100

    redirect homeController

--- week-10/ProjectRedditV2/src/main/java/com/greenfox/com/greenfox/rita/reddit/Controllers/HomeController.java
+++ week-10/ProjectRedditV2/src/main/java/com/greenfox/com/greenfox/rita/reddit/Controllers/HomeController.java
@@ -2,7 +2,6 @@ package com.greenfox.com.greenfox.rita.reddit.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Rita on 2017.01.05..
@@ -12,8 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String helloWorld() {
-        return "Hello World!";
+        return "redirect:/list";
     }
 }
